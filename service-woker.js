be9d893fbf5732b93a5357e5bfcab416336b86f2
kmkknweb <kongmakhonkaen@gmail.com>
2026-04-20 16:44:48 +0700

    Create service-woker.js

---
+++ service-woker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'kmkkn-pwa-v1';
+
+const urlsToCache = [
+  '/webpage/',
+  '/webpage/index.html',
+  '/webpage/manifest.json',
+  '/webpage/icon-192.png',
+  '/webpage/icon-512.png'
+];
+
+// install
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+// activate
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match('/webpage/index.html'))
+  );
+});
